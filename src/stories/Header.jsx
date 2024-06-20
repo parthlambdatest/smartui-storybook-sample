34e5e7cd425b5095698778c6b0e268f5e20eda8a
parthlambdatest <parthkirsan@lambdatest.com>
2024-06-20 16:23:02 +0530

    Testing Github PR check

--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -24,7 +24,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
             />
           </g>
         </svg>
-        <h1>Acme</h1>
+        <h1>Changes for Github PR</h1>
       </div>
       <div>
         {user ? (
